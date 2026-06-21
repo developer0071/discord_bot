ce242d47ac56ed008e5510c8e6ba75661b2ad168
Hunter <>
2026-06-22 00:30:41 +0500

    f

--- bot/bot.js
+++ bot/bot.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const cache = require('./src/cache');
